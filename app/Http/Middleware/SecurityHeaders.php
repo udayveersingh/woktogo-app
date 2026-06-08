0d2a98b5a5fdc63aee34e4fe4b75230e3a68fcc2
udayveersingh <uday.jbbn@gmail.com>
2026-06-08 15:50:48 +0530

    add security header file

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -21,7 +21,7 @@ class SecurityHeaders
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
 
         // Content Security Policy
-        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' https:; frame-src 'none'; object-src 'none'; base-uri 'self';");
+        // $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' https:; frame-src 'none'; object-src 'none'; base-uri 'self';");
 
         // Additional hardening
         $response->headers->set('X-XSS-Protection', '1; mode=block');
